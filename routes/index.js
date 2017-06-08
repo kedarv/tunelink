35488b54eeef678cc4d377148a50075f07ab12d0
Kedar V <kedar54@gmail.com>
2017-06-08 11:12:40 -0400

    use server IP

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 var client_id = '9c907cf58ae1447da90041a93960855a'; // Your client id
 var client_secret = '3b45951661b047e3aa3926df20a085dc'; // Your secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var redirect_uri = 'http://162.243.254.78:8888/callback'; // Your redirect uri
 
 var firebase = require('firebase');
 var config = {
@@ -135,7 +135,7 @@ router.post('/request', function(req, res, next) {
   let text = req.body.text;
   var message;
   if(text === "auth") {
-    message = "http://localhost:8888?user=" + req.body.user_name + '&id=' + req.body.user_id;
+    message = "http://162.243.254.78:8888?user=" + req.body.user_name + '&id=' + req.body.user_id;
   } else {
     search(req.body.user_name, text, function(parsed) {
       console.log(parsed);
