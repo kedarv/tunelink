17d79aedad3add452b0cf5a69017eb8fb9e34608
Kedar V <kedar54@gmail.com>
2017-06-08 11:28:10 -0400

    fix2

--- routes/index.js
+++ routes/index.js
@@ -144,7 +144,7 @@ router.post('/request', function(req, res, next) {
     var users = firebase.database().ref("users").once("value").then(function(snapshot) {
       snapshot.forEach(function(child) {
         if(child.val().slack_name === current_user && child.val().active === true) {
-          child.update({active: false});
+          firebase.database().ref('users/' + current_user + '/active').set(false);
         }
       })
     });
